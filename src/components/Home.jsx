18e1869b4fc408151ccd789cad9bcfe17f855456
maratsumi <dextroopsgaming@gmail.com>
2022-05-20 18:05:05 +0800

    moved button; changed text colors

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,10 +5,13 @@ const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#1d2126]">
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <h1 className="text-[#ec87e2] text-6xl sm:text-5xl font-bold">
+        <h1 className="text-[#e98adf] text-5xl font-bold text-center">
           ✿ Maratsumi ✿
         </h1>
-        <p className="text-[#ebebeb] text-3xl sm:text-xl py-6 max-w-[700px]">
+        <h2 className="text-[#fcbbd3] text-2xl sm:text-xl font-bold text-center">
+          Any Pronouns | Eng/Fil ok!
+        </h2>
+        <p className="text-[#ebebeb] text-3xl sm:text-xl py-6 max-w-[936px] text-center">
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
@@ -19,7 +22,7 @@ const Home = () => {
         </p>
         <div>
           <button
-            className="text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#e46fd8] hover:border-[#e46fd8] transition ease-in-out duration-300"
+            className="text-white border-2 px-6 py-3 my-2 mx-auto flex items-center hover:bg-[#e46fd8] hover:border-[#e46fd8] transition ease-in-out duration-300"
             style={{ borderRadius: "16px" }}
           >
             More About Me <FaRegArrowAltCircleRight className="ml-3" />
