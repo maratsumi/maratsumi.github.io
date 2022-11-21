324433af3072c469d25248a998937136cb55eade
maratsumi <dextroopsgaming@gmail.com>
2022-11-21 12:27:01 +0800

    removed item

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import WorkImg from "../assets/StudentInformationSystem.png";
-import redoThumb from "../assets/redothumbnail.png";
 import eftisfun from "../assets/tarkovisfunthumb.png";
 import covid19log from "../assets/COVID19Log.png";
 import oldwebsite from "../assets/OldWebsite.png";
@@ -20,27 +19,6 @@ const Work = () => {
           </h2>
         </div>
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4 items-center">
-          <div
-            style={{ backgroundImage: `url(${redoThumb})` }}
-            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto text-center content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100 transition ease-in duration-200">
-              <span className="text-2xl font-bold text-white tracking-wider">
-                Redo
-              </span>
-              <div className="pt-8 text-center">
-                <a
-                  href="https://youtu.be/r5Fv9onriMQ"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <button className="rounded-2xl border-2 border-gray-100 px-6 py-1 transform hover:scale-105 hover:bg-gray-100 hover:text-slate-800 hover:bg-opacity-90 hover:shadow-sm transition ease-in-out duration-150">
-                    YouTube
-                  </button>
-                </a>
-              </div>
-            </div>
-          </div>
           <div
             style={{ backgroundImage: `url(${eftisfun})` }}
             className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto text-center content-div"
